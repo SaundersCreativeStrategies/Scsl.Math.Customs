d9ae06f39cdd9ce8d901b49deac42ef2d31de2f8
Shane Saunders <asparatu@gmail.com>
2025-02-16 10:18:16 +0800

    feat: add AlcoholByVolume enum with proof conversion
    Added an AlcoholByVolume enum that converts ABV to proof. This standardizes alcohol strength representation.

---
+++ src/Scsl.Math.Customs/Enums/AlcoholByVolume.cs
@@ -0,0 +1,109 @@
+using Scsl.Attributes;
+
+namespace Scsl.Enums;
+
+public enum AlcoholByVolume
+{
+    [DisplayName("0.5%")]
+    HalfProof = 1,
+    [DisplayName("1%")]
+    OneProof = 2,
+    [DisplayName("2%")]
+    TwoProof = 4,
+    [DisplayName("3%")]
+    ThreeProof = 6,
+    [DisplayName("4%")]
+    FourProof = 8,
+    [DisplayName("5%")]
+    FiveProof = 10,
+    [DisplayName("6%")]
+    SixProof = 12,
+    [DisplayName("7%")]
+    SevenProof = 14,
+    [DisplayName("8%")]
+    EightProof = 16,
+    [DisplayName("9%")]
+    NineProof = 18,
+    [DisplayName("10%")]
+    TenProof = 20,
+    [DisplayName("11%")]
+    ElevenProof = 22,
+    [DisplayName("12%")]
+    TwelveProof = 24,
+    [DisplayName("13%")]
+    ThirteenProof = 26,
+    [DisplayName("14%")]
+    FourteenProof = 28,
+    [DisplayName("15%")]
+    FifteenProof = 30,
+    [DisplayName("16%")]
+    SixteenProof = 32,
+    [DisplayName("17%")]
+    SeventeenProof = 34,
+    [DisplayName("18%")]
+    EighteenProof = 36,
+    [DisplayName("19%")]
+    NineteenProof = 38,
+    [DisplayName("20%")]
+    TwentyProof = 40,
+    [DisplayName("21%")]
+    TwentyOneProof = 42,
+    [DisplayName("22%")]
+    TwentyTwoProof = 44,
+    [DisplayName("23%")]
+    TwentyThreeProof = 46,
+    [DisplayName("24%")]
+    TwentyFourProof = 48,
+    [DisplayName("25%")]
+    TwentyFiveProof = 50,
+    [DisplayName("26%")]
+    TwentySixProof = 52,
+    [DisplayName("27%")]
+    TwentySevenProof = 54,
+    [DisplayName("28%")]
+    TwentyEightProof = 56,
+    [DisplayName("29%")]
+    TwentyNineProof = 58,
+    [DisplayName("30%")]
+    ThirtyProof = 60,
+    [DisplayName("31%")]
+    ThirtyOneProof = 62,
+    [DisplayName("32%")]
+    ThirtyTwoProof = 64,
+    [DisplayName("33%")]
+    ThirtyThreeProof = 66,
+    [DisplayName("34%")]
+    ThirtyFourProof = 68,
+    [DisplayName("35%")]
+    ThirtyFiveProof = 70,
+    [DisplayName("36%")]
+    ThirtySixProof = 72,
+    [DisplayName("37%")]
+    ThirtySevenProof = 74,
+    [DisplayName("38%")]
+    ThirtyEightProof = 76,
+    [DisplayName("39%")]
+    ThirtyNineProof = 78,
+    [DisplayName("40%")]
+    FortyProof = 80,
+    [DisplayName("41%")]
+    FortyOneProof = 82,
+    [DisplayName("42%")]
+    FortyTwoProof = 84,
+    [DisplayName("43%")]
+    FortyThreeProof = 86,
+    [DisplayName("44%")]
+    FortyFourProof = 88,
+    [DisplayName("45%")]
+    FortyFiveProof = 90,
+    [DisplayName("46%")]
+    FortySixProof = 92,
+    [DisplayName("47%")]
+    FortySevenProof = 94,
+    [DisplayName("48%")]
+    FortyEightProof = 96,
+    [DisplayName("49%")]
+    FortyNineProof = 98,
+    [DisplayName("50%")]
+    FiftyProof = 100
+}
