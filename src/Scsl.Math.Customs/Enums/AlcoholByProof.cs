5a1cb191d23d70cd0a3fa0e76cb57010af421fd7
Shane Saunders <asparatu@gmail.com>
2025-02-16 10:25:42 +0800

    feat: add AlcoholByProof enum
    Added an enum to represent Alcohol By Proof (ABP) values.

---
+++ src/Scsl.Math.Customs/Enums/AlcoholByProof.cs
@@ -0,0 +1,207 @@
+using Scsl.Attributes;
+
+namespace Scsl.Enums;
+
+public enum AlcoholByProof
+{
+    [DisplayName("1 Proof")]
+    OneProof = 1,
+    [DisplayName("2 Proof")]
+    TwoProof = 2,
+    [DisplayName("3 Proof")]
+    ThreeProof = 3,
+    [DisplayName("4 Proof")]
+    FourProof = 4,
+    [DisplayName("5 Proof")]
+    FiveProof = 5,
+    [DisplayName("6 Proof")]
+    SixProof = 6,
+    [DisplayName("7 Proof")]
+    SevenProof = 7,
+    [DisplayName("8 Proof")]
+    EightProof = 8,
+    [DisplayName("9 Proof")]
+    NineProof = 9,
+    [DisplayName("10 Proof")]
+    TenProof = 10,
+    [DisplayName("11 Proof")]
+    ElevenProof = 11,
+    [DisplayName("12 Proof")]
+    TwelveProof = 12,
+    [DisplayName("13 Proof")]
+    ThirteenProof = 13,
+    [DisplayName("14 Proof")]
+    FourteenProof = 14,
+    [DisplayName("15 Proof")]
+    FifteenProof = 15,
+    [DisplayName("16 Proof")]
+    SixteenProof = 16,
+    [DisplayName("17 Proof")]
+    SeventeenProof = 17,
+    [DisplayName("18 Proof")]
+    EighteenProof = 18,
+    [DisplayName("19 Proof")]
+    NineteenProof = 19,
+    [DisplayName("20 Proof")]
+    TwentyProof = 20,
+    [DisplayName("21 Proof")]
+    TwentyOneProof = 21,
+    [DisplayName("22 Proof")]
+    TwentyTwoProof = 22,
+    [DisplayName("23 Proof")]
+    TwentyThreeProof = 23,
+    [DisplayName("24 Proof")]
+    TwentyFourProof = 24,
+    [DisplayName("25 Proof")]
+    TwentyFiveProof = 25,
+    [DisplayName("26 Proof")]
+    TwentySixProof = 26,
+    [DisplayName("27 Proof")]
+    TwentySevenProof = 27,
+    [DisplayName("28 Proof")]
+    TwentyEightProof = 28,
+    [DisplayName("29 Proof")]
+    TwentyNineProof = 29,
+    [DisplayName("30 Proof")]
+    ThirtyProof = 30,
+    [DisplayName("31 Proof")]
+    ThirtyOneProof = 31,
+    [DisplayName("32 Proof")]
+    ThirtyTwoProof = 32,
+    [DisplayName("33 Proof")]
+    ThirtyThreeProof = 33,
+    [DisplayName("34 Proof")]
+    ThirtyFourProof = 34,
+    [DisplayName("35 Proof")]
+    ThirtyFiveProof = 35,
+    [DisplayName("36 Proof")]
+    ThirtySixProof = 36,
+    [DisplayName("37 Proof")]
+    ThirtySevenProof = 37,
+    [DisplayName("38 Proof")]
+    ThirtyEightProof = 38,
+    [DisplayName("39 Proof")]
+    ThirtyNineProof = 39,
+    [DisplayName("40 Proof")]
+    FortyProof = 40,
+    [DisplayName("41 Proof")]
+    FortyOneProof = 41,
+    [DisplayName("42 Proof")]
+    FortyTwoProof = 42,
+    [DisplayName("43 Proof")]
+    FortyThreeProof = 43,
+    [DisplayName("44 Proof")]
+    FortyFourProof = 44,
+    [DisplayName("45 Proof")]
+    FortyFiveProof = 45,
+    [DisplayName("46 Proof")]
+    FortySixProof = 46,
+    [DisplayName("47 Proof")]
+    FortySevenProof = 47,
+    [DisplayName("48 Proof")]
+    FortyEightProof = 48,
+    [DisplayName("49 Proof")]
+    FortyNineProof = 49,
+    [DisplayName("50 Proof")]
+    FiftyProof = 50,
+    [DisplayName("51 Proof")]
+    FiftyOneProof = 51,
+    [DisplayName("52 Proof")]
+    FiftyTwoProof = 52,
+    [DisplayName("53 Proof")]
+    FiftyThreeProof = 53,
+    [DisplayName("54 Proof")]
+    FiftyFourProof = 54,
+    [DisplayName("55 Proof")]
+    FiftyFiveProof = 55,
+    [DisplayName("56 Proof")]
+    FiftySixProof = 56,
+    [DisplayName("57 Proof")]
+    FiftySevenProof = 57,
+    [DisplayName("58 Proof")]
+    FiftyEightProof = 58,
+    [DisplayName("59 Proof")]
+    FiftyNineProof = 59,
+    [DisplayName("60 Proof")]
+    SixtyProof = 60,
+    [DisplayName("61 Proof")]
+    SixtyOneProof = 61,
+    [DisplayName("62 Proof")]
+    SixtyTwoProof = 62,
+    [DisplayName("63 Proof")]
+    SixtyThreeProof = 63,
+    [DisplayName("64 Proof")]
+    SixtyFourProof = 64,
+    [DisplayName("65 Proof")]
+    SixtyFiveProof = 65,
+    [DisplayName("66 Proof")]
+    SixtySixProof = 66,
+    [DisplayName("67 Proof")]
+    SixtySevenProof = 67,
+    [DisplayName("68 Proof")]
+    SixtyEightProof = 68,
+    [DisplayName("69 Proof")]
+    SixtyNineProof = 69,
+    [DisplayName("70 Proof")]
+    SeventyProof = 70,
+    [DisplayName("71 Proof")]
+    SeventyOneProof = 71,
+    [DisplayName("72 Proof")]
+    SSeventyTwoProof = 72,
+    [DisplayName("73 Proof")]
+    SeventyThreeProof = 73,
+    [DisplayName("74 Proof")]
+    SeventyFourProof = 74,
+    [DisplayName("75 Proof")]
+    SeventyFiveProof = 75,
+    [DisplayName("76 Proof")]
+    SeventySixProof = 76,
+    [DisplayName("77 Proof")]
+    SeventySevenProof = 77,
+    [DisplayName("78 Proof")]
+    SeventyEightProof = 78,
+    [DisplayName("79 Proof")]
+    SeventyNineProof = 79,
+    [DisplayName("80 Proof")]
+    EightyProof = 80,
+    [DisplayName("81 Proof")]
+    EightyOneProof = 81,
+    [DisplayName("82 Proof")]
+    EightyTwoProof = 82,
+    [DisplayName("83 Proof")]
+    EightyThreeProof = 83,
+    [DisplayName("84 Proof")]
+    EightyFourProof = 84,
+    [DisplayName("85 Proof")]
+    EightyFiveProof = 85,
+    [DisplayName("86 Proof")]
+    EightySixProof = 86,
+    [DisplayName("87 Proof")]
+    EightySevenProof = 87,
+    [DisplayName("88 Proof")]
+    EightyEightProof = 88,
+    [DisplayName("89 Proof")]
+    EightyNineProof = 89,
+    [DisplayName("90 Proof")]
+    NinetyProof = 90,
+    [DisplayName("91 Proof")]
+    NinetyOneProof = 91,
+    [DisplayName("92 Proof")]
+    NinetyTwoProof = 92,
+    [DisplayName("93 Proof")]
+    NinetyThreeProof = 93,
+    [DisplayName("94 Proof")]
+    NinetyFourProof = 94,
+    [DisplayName("95 Proof")]
+    NinetyFiveProof = 95,
+    [DisplayName("96 Proof")]
+    NinetySixProof = 96,
+    [DisplayName("97 Proof")]
+    NinetySevenProof = 97,
+    [DisplayName("98 Proof")]
+    NinetyEightProof = 98,
+    [DisplayName("99 Proof")]
+    NinetyNineProof = 99,
+    [DisplayName("100 Proof")]
+    OneHundredProof = 100
+}
