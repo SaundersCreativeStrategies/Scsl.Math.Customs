f957ed5d8ad77b356b293e0a4933c85f679630d8
Shane Saunders <asparatu@gmail.com>
2025-02-16 12:07:51 +0800

    feat(enum): add BottleSize enum
    Added a BottleSize enum to represent different bottle sizes.

---
+++ src/Scsl.Math.Customs/Enums/BottleSize.cs
@@ -0,0 +1,48 @@
+using Scsl.Attributes;
+
+namespace Scsl.Enums;
+
+public enum BottleSize
+{
+    [DisplayName("50ml")]
+    BottleSize50Ml = 50,
+
+    [DisplayName("187ml")]
+    BottleSize187Ml = 187,
+
+    [DisplayName("200ml")]
+    BottleSize200Ml = 200,
+
+    [DisplayName("330ml")]
+    BottleSize330Ml = 330,
+
+    [DisplayName("375ml")]
+    BottleSize375Ml = 375,
+
+    [DisplayName("500ml")]
+    BottleSize500Ml = 500,
+
+    [DisplayName("650ml")]
+    BottleSize650Ml = 650,
+
+    [DisplayName("750ml")]
+    BottleSize750Ml = 750,
+
+    [DisplayName("1000ml")]
+    BottleSize1000Ml = 1000,
+
+    [DisplayName("1500ml")]
+    BottleSize1500Ml = 1500,
+
+    [DisplayName("1750ml")]
+    BottleSize1750Ml = 1750,
+
+    [DisplayName("1892ml")]
+    BottleSize1892Ml = 1892,
+
+    [DisplayName("3000ml")]
+    BottleSize3000Ml = 3000,
+    
+    [DisplayName("6000ml")]
+    BottleSize6000Ml = 6000
+}
